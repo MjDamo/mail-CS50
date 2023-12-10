36e66693d68a32274c1854556485782d2a7e598c
Mj <142387440+MjDamo@users.noreply.github.com>
2023-12-10 05:09:26 -0800

    finish send mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // Submit compose-form Listener
+  document.querySelector('#compose-form').addEventListener('submit', send_email);
+
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -30,4 +33,30 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+}
+
+function send_email(event) {
+  event.preventDefault();
+  // email fields
+  const compose_recipients = document.querySelector('#compose-recipients').value;
+  const compose_subject = document.querySelector('#compose-subject').value;
+  const compose_body = document.querySelector('#compose-body').value;
+
+
+  // Sending data
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: compose_recipients,
+        subject: compose_subject,
+        body: compose_body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    // Print result
+    console.log(result);
+    load_mailbox('sent')
+  });
 }
